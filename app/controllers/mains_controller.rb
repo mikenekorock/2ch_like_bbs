ddc5d730487e579d8644d0a90f54c16553a9ffe3
mikeneko <golden.lucky777@gmail.com>
2014-05-23 14:20:29 +0900

    Initial commit

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,5 +1,26 @@
 class MainsController < ApplicationController
 	def index
 		@mains = Main.all
+		@main = Main.new
 	end
+
+
+	def new
+		@main = Main.new
+	end
+
+
+	def create
+		@main = Main.new(main_params)
+		@main.save
+		redirect_to mains_path
+	end
+
+
+	private
+
+		def main_params
+			params[:main].permit(:title, :name, :text)
+		end
+
 end
